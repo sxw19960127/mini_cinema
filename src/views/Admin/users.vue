f6058fa11cd552ec563d2c1c0a30fafb5ec3f87d
1183476700@qq.com <1183476700@qq.com>
2020-02-01 21:58:01 +0800

    2.1

--- src/views/Admin/users.vue
+++ src/views/Admin/users.vue
@@ -1,13 +1,62 @@
 <template>
-  <div>用户</div>
+  <div>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="date" label="注册日期"></el-table-column>
+      <el-table-column prop="username" label="用户姓名"></el-table-column>
+      <el-table-column prop="email" label="用户邮箱"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button size="small" @click="handleToFreeze(scope.$index, scope.row)">{{ scope.row.isFreeze ? '已冻结' : '未冻结' }}</el-button>
+          <el-button size="small" type="danger">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "users",
+  data() {
+    return {
+      tableData: []
+    };
+  },
+  mounted() {
+    this.axios.get("/api2/admin/usersList").then(res => {
+      // console.log(res)
+      var status = res.data.status;
+      if (status === 0) {
+        this.tableData = res.data.data.usersList;
+      }
+    });
+  },
+  methods: {
+    handleToFreeze(index,row) {
+      // console.log(index, row)
+      this.axios.post('/api2/admin/updateFreeze', {
+        email: row.email,
+        isFreeze: !row.isFreeze
+      }).then(res => {
+        var status = res.data.status;
+        if(status === 0) {
+          // 下面代码我们选择了使用element-ui提供的弹窗
+          this.$alert('冻结操作已成功', '信息', {
+            confirmButtonText: '确定',
+            callback: action => {
+              this.tableData[index].isFreeze = !row.isFreeze
+            }
+          });
+        }else {
+          this.$alert('冻结操作失败', '信息', {
+            confirmButtonText: '确定',
+          });
+        }
+      })
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
--- src/views/Mine/center.vue
+++ src/views/Mine/center.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
      <div>个人中心：</div>
-     <div>当前用户：{{ $store.state.user.name }} <a href="javascript:;" @touchstart="handleToLogin">退出</a></div>
+     <div>当前用户：{{ $store.state.user.name }} <a href="javascript:;" @touchstart="handleToLogout">退出</a></div>
      <div v-if="$store.state.user.isAdmin">用户身份：管理员 <a href="/admin" target="_blank">进入后台管理系统</a></div>
      <div v-else>用户身份：普通会员</div>
   </div>
@@ -12,12 +12,12 @@ import axios from 'axios'
 export default {
    name: 'center',
    methods: {
-      handleToLogin() {
+      handleToLogout() {
          this.axios.get('/api2/users/logout').then(res => {
             var status = res.data.status
             if(status === 0) {
-               localStorage.removeItem('name', res.data.data.username) // 将username进行本地存储
-               localStorage.removeItem('isAdmin', res.data.data.isAdmin) // 将Admin也进行本地存储一下
+               localStorage.removeItem('name') // 将username进行本地存储
+               localStorage.removeItem('isAdmin') // 将Admin也进行本地存储一下
                this.$store.commit('user/USER_NAME', {name: '' , isAdmin: false}) // 当我们退出的时候,将用户名置空
                this.$router.push('/mine/login')
             }
